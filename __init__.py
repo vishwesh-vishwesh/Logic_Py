26cd960aa52c8cbba32d4eb4678797e73cd630d3
Vishwesh <36016864+vishwesh-vishwesh@users.noreply.github.com>
2021-06-12 18:53:19 +0530

    secondary gates imported

--- __init__.py
+++ __init__.py
@@ -5,4 +5,6 @@ Created on Thu Jun 10 17:34:19 2021
 @author: Vishwesh
 """
 from basic_gates import AND, OR, NOT, NAND, NOR,XNOR,XOR
-
+from secondary_gates import AND_AND, AND_OR, AND_NAND, AND_NOR, OR_AND, OR_OR 
+from secondary_gates import OR_NAND, OR_NOR, NAND_AND, NAND_OR, NAND_NAND, NAND_NOR 
+from secondary_gaates import NOR_AND, NOR_OR, NOR_NAND, NOR_NOR
