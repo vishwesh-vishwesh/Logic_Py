4ab439be1ba7a8b06fb58d349326765c9d925da3
Vishwesh <36016864+vishwesh-vishwesh@users.noreply.github.com>
2021-06-14 05:23:24 +0530

    imports
    Combination gates, secondary gates, plots

--- __init__.py
+++ __init__.py
@@ -7,4 +7,6 @@ Created on Thu Jun 10 17:34:19 2021
 from basic_gates import AND, OR, NOT, NAND, NOR,XNOR,XOR
 from secondary_gates import AND_AND, AND_OR, AND_NAND, AND_NOR, OR_AND, OR_OR 
 from secondary_gates import OR_NAND, OR_NOR, NAND_AND, NAND_OR, NAND_NAND, NAND_NOR 
-from secondary_gaates import NOR_AND, NOR_OR, NOR_NAND, NOR_NOR
+from secondary_gates import NOR_AND, NOR_OR, NOR_NAND, NOR_NOR
+from arithmatic_gates import half_adder,full_adder
+from plotting import plot_full_adder, plot_half_adder, plot_secondary, plot_basic
